a3fc4e4e44cfb9e7c2acb77987904fc70024d505
Kononovaele <Kononovaele@yandex.ru>
2023-01-08 19:56:02 +0300

    do task_2, code and schema

---
+++ task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿//
+// Напишите задачу, которая принимает на вход два числа и выдает большее.
+//
+
+Console.WriteLine("--- Task_2 ---");
+
+int a = 5;
+int b = 7;
+if(a > b) {
+    Console.Write("a: ");
+    Console.WriteLine(a);
+}
+else {
+    Console.Write("b: ");
+    Console.WriteLine(b);
+}
+
+a = 2;
+b = 10;
+if(a > b) {
+    Console.Write("a: ");
+    Console.WriteLine(a);
+}
+else {
+    Console.Write("b: ");
+    Console.WriteLine(b);
+}
+
+a = -9;
+b = -3;
+if(a > b) {
+    Console.Write("a: ");
+    Console.WriteLine(a);
+}
+else {
+    Console.Write("b: ");
+    Console.WriteLine(b);
+}
+
